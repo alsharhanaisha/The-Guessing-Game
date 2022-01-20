da813023e34467975b2984e6e9f8a5b95f61c61b
alsharhanaisha <alsharhanaisha@gmail.com>
2022-01-20 16:38:26 +0300

    Done 2

--- guessinggame/src/App.js
+++ guessinggame/src/App.js
@@ -9,11 +9,12 @@ function App() {
   const [guessbtn, setGuessBtn] = useState("");
   const [number, setNumber] = useState(0);
   const [attempt, setAttempt] = useState(0);
-  const [round, setRound] = useState(getRandomNumber);
+  const [randomNumber, setRandomNumber] = useState(getRandomNumber);
+  // const [hints, setHints] = useState();
 
   const GuessButton = () => {
     setGuessBtn(number);
-    incrementAttempt();
+    winningGame();
   };
 
   const typeNumber = (event) => {
@@ -22,29 +23,50 @@ function App() {
 
   const incrementAttempt = () => {
     setAttempt(attempt + 1);
-    if (attempt === 4) {
-      alert("LOSER! .... let's restart the game ");
-      newRound();
-    }
   };
 
   const newRound = () => {
     setAttempt(0);
-    setRound(getRandomNumber());
+    setRandomNumber(getRandomNumber());
   };
+
+  const winningGame = () => {
+    incrementAttempt();
+    if (number == randomNumber) {
+      alert("Winner Winner! Chicken Dinner!");
+      newRound();
+    } else if (attempt === 4) {
+      alert("LOSER! .... let's restart the game ");
+      newRound();
+    } else {
+      alert(`WRONG! only ${4 - attempt} attempts left`);
+    }
+  };
+
+  // const getHints = () => {
+  //   //retreive randomNumber
+  //   let hintNumbers = [randomNumber];
+  //   hintNumbers.push(getRandomNumber());
+
+  //   //display an array of hintNumbers that takes in randomNumber as the first element
+  //   //getRandomNumber that takes 5 digits less than the randomNumber and 5 digits less than
+  // };
 
   return (
     <div className="App">
-      <header>Aisha's Guessing Game</header>
+      <header className="titleheader">
+        <h1>Aisha's Guessing Game</h1>
+      </header>
       <input
         className="inputholder"
-        placeholder="Guess any number between 0-100"
+        placeholder="Guess any number between 0 - 100"
         type="number"
         onChange={(event) => typeNumber(event)}
       />
-      <button onClick={GuessButton} className="button-class">
+      <button onClick={GuessButton} className="submit-button">
         Submit Your Guess
       </button>
+      <button className="hint-button">Hint ?</button>
     </div>
   );
 }
